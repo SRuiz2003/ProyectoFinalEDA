3da7820c851b2a698c411dd7c492024af5705cc0
Santiago Cortés <109761550+scortesg1@users.noreply.github.com>
2023-05-13 10:34:49 -0500

    Comentarios de complejidad resolver torneo

--- js/utils.js
+++ js/utils.js
@@ -16,15 +16,17 @@ function resolverTorneo(torneo) {
 
     try {
 
+        // Sea k la longitud de la cadena a separar en el metodo split
+
         //Word Cup 1998 – Group A--Brazil,Norway,Morocco,Scotland-- Brazil#2#1#Scotland,Norway#2#2#Morocco,Scotland#1#1#Norway,/////Brazil#3#0#Morocco, Morocco#3#0#Scotland,Brazil#1#2#Norway
         
-        const info = torneo.split('--');
+        const info = torneo.split('--'); // Complejidad: O(k) 
         // [Word Cup 1998 – Group A, Brazil,Norway,Morocco,Scotland, Brazil#2#1#Scotland,Norway#2#2#Morocco,Scotland#1#1#Norway,Brazil#3#0#Morocco, Morocco#3#0#Scotland,Brazil#1#2#Norway]
         
-        const equipos = info[1].split(','); 
+        const equipos = info[1].split(','); // Complejidad: O(k)
         //[Brazil,Norway,Morocco,Scotland]
 
-        const encuentros = info[2].split(',').map(partido => partido.split('#').reverse());
+        const encuentros = info[2].split(',').map(partido => partido.split('#').reverse()); // Complejidad: O(k)
 
         // Encuentros es una matriz de 4 columnas donde la primera y la ultima son los equipos y la segunda y tercera los goles respectivamente
         //      0      1    2       3
@@ -35,8 +37,9 @@ function resolverTorneo(torneo) {
         // ['Norway', '2', '2', 'Morocco']
         // ['Brazil', '2', '1', 'Scotland']
 
-        // Para calcular la complejidad habrá que usar un for convencional el entries y el arreglo [indice, equipo] es para poner el indice en la respuesta el final.
+        // El entries y el arreglo [indice, equipo] es para poner el indice en la respuesta al final.
 
+<<<<<<< Updated upstream
         for(const [indice, equipo] of equipos.entries()){
 
             // Se descartan las variables calculables
@@ -48,34 +51,52 @@ function resolverTorneo(torneo) {
             let golesEnContra = 0;
 
             let linea = '';
+=======
+        // Agregar titulo del torneo a la salida
+        campoSalida.value+= `${info[0]}\n` // Complejidad: O(1)
 
-            for (let i = 0; i < encuentros.length; i++) {
+        const final  = [] // Complejidad: O(1)
+
+        // Arreglo para contener los equipos finales con toda su informacion y ordenarlos
+        for(let equipo of equipos){ // Complejidad: O(n)
+
+            // Se descartan las variables calculables
+            let cantidadJuegos = 0; // Complejidad: O(1)
+            let victorias = 0; // Complejidad: O(1)
+            let empates = 0; // Complejidad: O(1)
+            let derrotas = 0; // Complejidad: O(1)
+            let golesMarcados = 0; // Complejidad: O(1)
+            let golesEnContra = 0; // Complejidad: O(1)
+>>>>>>> Stashed changes
+
+            for (let i = 0; i < encuentros.length; i++) { // Complejidad: O(m)
                 // Caso 1 ['Scotland', '1', '1', 'Norway']
-                if (encuentros[i][0] == equipo) {
-                    cantidadJuegos++;
-                    golesMarcados += parseInt(encuentros[i][1]);
-                    golesEnContra += parseInt(encuentros[i][2]);
-                    if(encuentros[i][1] > encuentros[i][2]){
+                if (encuentros[i][0] == equipo) { // Complejidad: O(1)
+                    cantidadJuegos++; // Complejidad: O(1)
+                    golesMarcados += parseInt(encuentros[i][1]); // Complejidad: O(1)
+                    golesEnContra += parseInt(encuentros[i][2]); // Complejidad: O(1)
+                    if(encuentros[i][1] > encuentros[i][2]){ // Complejidad: O(1)
                         victorias++;
-                    }else if(encuentros[i][1] < encuentros[i][2]){
-                        derrotas++;
+                    }else if(encuentros[i][1] < encuentros[i][2]){ // Complejidad: O(1)
+                        derrotas++; // Complejidad: O(1)
                     }else{
-                        empates++;
+                        empates++; // Complejidad: O(1)
                     }
                 // Caso 2 ['Morocco', '3', '0', 'Scotland']
-                }else if(encuentros[i][3] == equipo){
-                    cantidadJuegos++;
-                    golesMarcados += parseInt(encuentros[i][2]);
-                    golesEnContra += parseInt(encuentros[i][1]);
-                    if(encuentros[i][2] > encuentros[i][1]){
-                        victorias++;
-                    }else if(encuentros[i][2] < encuentros[i][1]){
-                        derrotas++;
+                }else if(encuentros[i][3] == equipo){ // Complejidad: O(1)
+                    cantidadJuegos++; // Complejidad: O(1)
+                    golesMarcados += parseInt(encuentros[i][2]); // Complejidad: O(1)
+                    golesEnContra += parseInt(encuentros[i][1]); // Complejidad: O(1)
+                    if(encuentros[i][2] > encuentros[i][1]){ // Complejidad: O(1)
+                        victorias++; // Complejidad: O(1)
+                    }else if(encuentros[i][2] < encuentros[i][1]){ // Complejidad: O(1)
+                        derrotas++; // Complejidad: O(1)
                     }else{
-                        empates++;
+                        empates++; // Complejidad: O(1)
                     }
                 }
             }
+<<<<<<< Updated upstream
 
             // Formato de salida: Nombre del equipo, Puntos del equipo[p], Cantidad de juegos[g], (victorias-empates-derrotas), diferencia de goles[gd] (Goles marcados - Goles en contra)
 
@@ -85,16 +106,52 @@ function resolverTorneo(torneo) {
 
         }
 
+=======
+            let difGoles = golesMarcados - golesEnContra;// Complejidad: O(1)
+            let EqObj = { // Complejidad: O(1)
+                equipo:equipo,
+                puntos:(victorias*3)+empates,
+                cantidadJuegos:cantidadJuegos,
+                victorias:victorias,
+                empates:empates,
+                derrotas:derrotas,
+                golesMarcados:golesMarcados,
+                golesEnContra:golesEnContra,
+                difGoles: difGoles
+            }
+            final.push(EqObj);// Complejidad: O(1)
+
+        }
+        let linea = ''; // Complejidad: O(1)
+
+        sortEquipos(final); // Complejidad: ? 
+        console.log(final); // Complejidad: O(1)
+
+         // Formato de salida: Nombre del equipo, Puntos del equipo[p], Cantidad de juegos[g], (victorias-empates-derrotas), diferencia de goles[gd] (Goles marcados - Goles en contra)
+         
+        let i = 1; // Complejidad: O(1)
+        final.forEach(element => {
+            linea += `${i}) ${element.equipo} ${element.puntos}p, ${element.cantidadJuegos}g (${element.victorias}-${element.empates}-${element.derrotas}), ${element.difGoles}gd (${element.golesMarcados}-${element.golesEnContra})\n`; // Complejidad: O(1)
+            i++; // Complejidad: O(1)
+          });
+          
+        campoSalida.value += linea; // Complejidad: O(1)
+>>>>>>> Stashed changes
     } catch (TypeError) {
-        alert('El campo de entrada está vacio');
+        alert('El campo de entrada está vacio'); // Complejidad: O(1)
     }
         
 }
 
 function limpiarCampo() {
+<<<<<<< Updated upstream
 >>>>>>> Stashed changes
     campoEntrada.value = "";
     campoSalida.value = "";
+=======
+    campoEntrada.value = ""; // Complejidad: O(1)
+    campoSalida.value = ""; // Complejidad: O(1)
+>>>>>>> Stashed changes
 }
 
 <<<<<<< Updated upstream
